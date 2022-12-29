2c6b28756128636ee21b41a05ca646bb0039dde2
kaori221b <108169241+kaori221b@users.noreply.github.com>
2022-12-29 21:13:38 +0900

    条件分岐

--- chap2_6.rb
+++ chap2_6.rb
@@ -1,6 +1,10 @@
 #複数の条件式を組み合わせる
 puts "年齢は？"
 age = gets.to_i
-if age >= 6 && age <= 15
-    puts "義務教育の対象"
+if age <= 5
+    puts "未就学児"    
+    elsif
+        age >= 6 && age <= 15
+        puts "義務教育の対象"
+
 end
